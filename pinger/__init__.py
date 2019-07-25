5c05f8ef997e0b50936c92ee1312fc027150619b
rinkkyjia@gmail.com <rinkkyjia@gmail.com>
2019-07-26 00:39:38 +0800

    add support for macOS and Linux

--- pinger/__init__.py
+++ pinger/__init__.py
@@ -1,26 +1,24 @@
 import re
 import os
+import math
 
-RE_PING_SPEED = re.compile(r'=(\d+)ms')
+if os.name == 'posix':
+    RE_PING_SPEED = re.compile(r'=\s*(\d+\.?\d*)/')
+    PING_CMD = 'ping -c 1 -W {wait} {ip}'
+else:
+    RE_PING_SPEED = re.compile(r'=(\d+)ms')
+    PING_CMD = 'ping /n 1 /w {wait} {ip}'
 
-def ping_test(ip, wait=50):
-    cmd_out = os.popen(
-        'ping /n 1 /w {wait} {ip}'.format(ip=ip, wait=wait)
-    ).read()
-    return not ('100%' in cmd_out)
-
-def ping_time(ip, wait=50):
-    cmd_out = os.popen('ping /n 1 /w {wait} {ip}'.format(
-        ip=ip, wait=wait
-    )).read()
+def ping_time(ip, wait=100):
+    cmd_out = os.popen(PING_CMD.format(ip=ip, wait=wait)).read()
     match = RE_PING_SPEED.search(cmd_out)
     try:
         spend = match.group(1)
-        return int(spend)
+        return math.ceil(float(spend))
     except:
         return 0
 
-def filter_and_sort(ip_list, wait=50, save_to=None):
+def filter_and_sort(ip_list, wait=100, save_to=None):
     ip_time_dict = {}
     if save_to:
         with open(save_to, 'w') as file:
