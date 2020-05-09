a646cf0bd2cabd239cb64c3885084a457e36dcc4
rinkky <rinkky@live.com>
2020-05-10 03:52:25 +0800

    update ip list

--- xbox/__init__.py
+++ xbox/__init__.py
@@ -3,7 +3,7 @@ import requests
 
 curl_cmd = 'curl'
 url_postfix='/5/795514b6-aad9-4c1c-ac2a-60c1492d7f31/0c57204f-f4f0-4bf6-b119-b7afc231994d/0.0.61375.0.6574fcb5-72f2-4c85-98c1-bd1059c79934/Destiny2_0.0.61375.0_neutral__z7wx9v9k22rmg'
-curl_range='33543139328-33574596608'
+curl_range='33543139328-33606053888'
 
 def download_speed(ip):
     """
